fbeab1b42ddd2737f28f57e7b6e37086379c9c68
emallson <emallson@atlanis.net>
2020-04-04 02:47:16 -0400

    bumping dep version

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,25 @@
 extern crate capnp;
 extern crate petgraph;
 
-use petgraph::{Direction, Graph};
 use petgraph::visit::EdgeRef;
+use petgraph::{Direction, Graph};
 
 pub mod graph_capnp {
     include!(concat!(env!("OUT_DIR"), "/graph_capnp.rs"));
 }
-use graph_capnp::{graph_header, edge};
 use capnp::serialize_packed;
+use graph_capnp::{edge, graph_header};
 
-use std::io::{BufReader, BufWriter, Error};
 use std::fs::File;
+use std::io::{BufReader, BufWriter, Error};
 
 pub type NodeId = u32;
 
 pub fn load_edges(p: &str) -> capnp::Result<Vec<(NodeId, NodeId, f32)>> {
-    let f = try!(File::open(p));
+    let f = File::open(p)?;
     let mut reader = BufReader::new(f);
     let opts = ::capnp::message::ReaderOptions::new();
-    let msg = try!(serialize_packed::read_message(&mut reader, opts));
+    let msg = serialize_packed::read_message(&mut reader, opts)?;
     let header_root: graph_header::Reader = msg.get_root().unwrap();
     let mut edges = Vec::with_capacity(header_root.get_num_edges() as usize);
 
@@ -27,23 +27,18 @@ pub fn load_edges(p: &str) -> capnp::Result<Vec<(NodeId, NodeId, f32)>> {
         let edge_root: edge::Reader = msg.get_root().unwrap();
 
         let mut edgelist = match edge_root.get_to().which().unwrap() {
-            edge::to::Node(to) => {
-                match edge_root.get_weight().which().unwrap() {
-                    edge::weight::Value(weight) => vec![(edge_root.get_from(), to, weight)],
-                    _ => panic!("Single node given with List of Weights"),
-                }
-            }
-            edge::to::List(Ok(to_list)) => {
-                match edge_root.get_weight().which().unwrap() {
-                    edge::weight::List(Ok(w_list)) => {
-                        to_list.iter()
-                            .zip(w_list.iter())
-                            .map(|(to, w)| (edge_root.get_from(), to, w))
-                            .collect::<Vec<(u32, u32, f32)>>()
-                    }
-                    _ => panic!("Single weight given with List of Nodes"),
-                }
-            }
+            edge::to::Node(to) => match edge_root.get_weight().which().unwrap() {
+                edge::weight::Value(weight) => vec![(edge_root.get_from(), to, weight)],
+                _ => panic!("Single node given with List of Weights"),
+            },
+            edge::to::List(Ok(to_list)) => match edge_root.get_weight().which().unwrap() {
+                edge::weight::List(Ok(w_list)) => to_list
+                    .iter()
+                    .zip(w_list.iter())
+                    .map(|(to, w)| (edge_root.get_from(), to, w))
+                    .collect::<Vec<(u32, u32, f32)>>(),
+                _ => panic!("Single weight given with List of Nodes"),
+            },
             _ => panic!("Unable to read edge"),
         };
 
@@ -58,7 +53,7 @@ pub fn load_graph(p: &str) -> capnp::Result<Graph<(), f32>> {
 }
 
 pub fn write_graph(p: &str, tag: &str, g: &Graph<(), f32>) -> Result<(), Error> {
-    let f = try!(File::create(p));
+    let f = File::create(p)?;
     let mut writer = BufWriter::new(f);
 
     // write header
@@ -70,7 +65,7 @@ pub fn write_graph(p: &str, tag: &str, g: &Graph<(), f32>) -> Result<(), Error> 
             header.set_num_nodes(g.node_count() as u32);
             header.set_num_edges(g.edge_count() as u64);
         }
-        try!(serialize_packed::write_message(&mut writer, &message));
+        serialize_packed::write_message(&mut writer, &message)?;
     }
 
     for node in g.node_indices() {
@@ -81,19 +76,19 @@ pub fn write_graph(p: &str, tag: &str, g: &Graph<(), f32>) -> Result<(), Error> 
             let mut em = message.init_root::<edge::Builder>();
             em.set_from(node.index() as u32);
             {
-                let mut to = em.borrow().get_to().init_list(num_edges);
+                let mut to = em.reborrow().get_to().init_list(num_edges);
                 for (i, edgeref) in edges.clone().enumerate() {
                     to.set(i as u32, edgeref.target().index() as u32);
                 }
             }
             {
-                let mut weight = em.borrow().get_weight().init_list(num_edges);
+                let mut weight = em.reborrow().get_weight().init_list(num_edges);
                 for (i, edgeref) in edges.clone().enumerate() {
                     weight.set(i as u32, *edgeref.weight());
                 }
             }
         }
-        try!(serialize_packed::write_message(&mut writer, &message));
+        serialize_packed::write_message(&mut writer, &message)?;
     }
     Ok(())
 }
